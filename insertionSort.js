26a3a4a69bcf62faa2066842d10e7b682b92bcdf
m2paulc <hyp3rsp8c3@gmail.com>
2019-05-27 01:59:07 -0400

    fixed reference error by def variables in the beginning

--- insertionSort.js
+++ insertionSort.js
@@ -1,12 +1,13 @@
 function insertionSort(arr) {
-  let currentVal;
-  for (let i = 1; i < arr.length; i++) {
+  let currentVal, i, j;
+  for (i = 1; i < arr.length; i++) {
     currentVal = arr[i];
-    for (let j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+    for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       arr[j + 1] = arr[j];
     }
     arr[j + 1] = currentVal;
   }
+  console.log(arr);
   return arr;
 }
 
